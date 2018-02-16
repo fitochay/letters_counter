0d23a6dc6522908a4f7973c1fe25dc2f3c268d0a
Ilya <ilspas15@gmail.com>
2018-02-16 14:36:05 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -1,119 +1,99 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: tproo
- *
- * Created on February 14, 2018, 12:02 AM
- */
-
-#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <map>
-#include <cwchar>
-#include <string>
-#include <clocale>
 
 using namespace std;
 
-/*
- * 
- */
-
 class counter
 {
-	string r_filename, w_filename;
-	map<wchar_t, float> counters;
-	int number;
+    locale *loc;
+    string r_filename, w_filename;
+    map<wchar_t, float> counters;
+    int number;
 
-	public:
-	
-	counter()
-	{
-		number = 0;
-	}
+    public:
 
-	void set_filenames();
+    counter()
+    {
+        number = 0;
+        loc = new locale("");
+    }
 
-	void write_to_file();
+    void set_filenames();
 
-	void count_letters();
+    void write_to_file();
 
-	void print_counter()
-	{
-		for (auto& x:counters)
-		{
-			x.second /= number;
-			wcout << x.first << L";" << x.second << endl;
-		}
-	}	
+    void count_letters();
+    
+    void print_map();
 };
 
 void counter::set_filenames()
 {
-	this->r_filename = "file";
-	this->w_filename = "file2";
+    r_filename = "file";
+    w_filename = "file2";
 }
 
 void counter::write_to_file()
 {
-	wofstream file;
-	file.open(this->w_filename);
-	for (auto& x:counters)
-	{
-		x.second /= number;
-		file << x.first << L"; " << x.second << endl;
-	}
-	file.close();
+    wofstream stream;
+    stream.open(w_filename);
+    stream.imbue(*loc);
+    for (auto& x:counters)
+    {
+        x.second /= number;
+        stream << x.first << L"; " << x.second << endl;
+    }
+    stream.close();
 }
 
 void counter::count_letters()
 {
-	wifstream file(this->r_filename);
-    locale loc("");
-	if (file.is_open())
-	{
-        file.imbue(loc);
-		wchar_t ch_low;
+    wifstream stream(this->r_filename);
+    if (stream.is_open())
+    {
+        stream.imbue(*loc);
+        wchar_t ch_low;
         wstring wstr;
-		while (getline(file, wstr))
-		{
-        cout << " ";
-			for (int i = 0; i < wstr.size(); i ++)
-			{
-				if (wstr[i] >= L'А' && wstr[i] <= L'Я')
-				{
-					number ++;
-                    //ch_low = tolower(wstr[i], locale("ru_RU.UTF-8"));
+        while (getline(stream, wstr))
+        {
+            for (int i = 0; i < wstr.size(); i ++)
+            {
+                if (wstr[i] >= L'А' && wstr[i] <= L'Я')
+                {
+                    number ++;
                     ch_low = wstr[i];
-					if (counters.count(ch_low) != 0)
-					{
-						counters[ch_low] ++;
-					}
-					else
-					{
-						counters.insert(pair<wchar_t,int>(ch_low, 1));
-					}
-				}
-			}
-		}
-		file.close();
-	}
+                    if (counters.count(ch_low) != 0)
+                    {
+                        counters[ch_low] ++;
+                    }
+                    else
+                    {
+                        counters.insert(pair<wchar_t,int>(ch_low, 1));
+                    }
+                }
+            }
+        }
+        stream.close();
+    }
+}
+
+void counter::print_map()
+{
+    ios_base::sync_with_stdio(false);
+    wcout.imbue(*loc);
+    for (auto& x:counters)
+    {
+        wcout << x.first << L"; " << x.second << endl;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	counter c;
-    //setlocale(LC_ALL, "ru_RU.UTF-8");
-    setlocale(LC_ALL, "");
-    
-        c.set_filenames();
-        c.count_letters();
-        c.write_to_file();
-	return 0;
+    counter c;
+    c.set_filenames();
+    c.count_letters();
+    c.write_to_file();
+    c.print_map();
+    return 0;
 }
